a84612493045db98d77bdd31fc1f5186a48c2c2e
jrubacha <jrubacha@lsc.k12.in.us>
2021-04-22 12:59:03 -0400

    so many space facts

--- Talkies.java
+++ Talkies.java
@@ -5,20 +5,28 @@ public class Talkies {
     int currentDay = 0;
 
     private String lonely = "No one wants to talk to you today.";
-    private String fact1 = "Random space fact 1";
-    private String fact2 = "random space fact 2";
-    private String fact3 = "random space fact 3";
+    private String fact1 = "True to its namesake (the speedy messenger of ancient Roman gods), Mercury is the fastest planet in our solar system. It zips around our Sun at an average of 172,000 kilometers per hour (107,000 miles per hour) — about 65,000 kph (40,000 mph) faster than Earth. A year on Mercury is equal to 88 Earth days.";
+    private String fact2 = "Neptune’s winds are the fastest in the solar system, reaching 2,575 kilometers per hour (1,600 miles per hour)! Neptune’s giant, spinning storms could swallow the whole Earth.";
+    private String fact3 = "Saturn is the only planet in our solar system that is less dense than water. It could float in a bathtub if anybody could build a bathtub big enough.";
     private String fact4 = "random space fact 4";
     private String fact5 = "random space fact 5";
     private String fact6 = "random space fact 6";
     private String fact7 = "random space fact 7";
     private String fact8 = "random space fact 8";
     private String fact9 = "random space fact 9";
-    private String moonFact1 = "random moon fact 1";
-    private String moonFact2 = "random moon fact 2";
-    private String moonFact3 = "random moon fact 3";
-    private String moonFact4 = "random moon fact 4";
-    private String moonFact5 = "random moon fact 5";
+    private String moonFact1 = "Craters at the Moon’s south pole may be the frostiest locale in the entire solar system. In the permanently shadowed crater floors, “daytime” temperatures may never rise above minus 238 degrees Celsius (minus 397 degrees Fahrenheit).";
+    private String moonFact2 = "The Moon is drifting away from the Earth. The Moon is moving approximately 3.8 cm away from our planet every year. It is estimated that it will continue to do so for around 50 billion years. By the time that happens, the Moon will be taking around 47 days to orbit the Earth instead of the current 27.3 days.";
+    private String moonFact3 = "The Moon has quakes. These are caused by the gravitational pull of the Earth. Lunar astronauts used seismographs on their visits to the Moon, and found that small moonquakes occurred several kilometres beneath the surface, causing ruptures and cracks. Scientists think the Moon has a molten core, just like Earth.";
+    private String moonFact4 = "The Moon is the fifth largest natural satellite in the Solar System. At 3,475 km in diameter, the Moon is much smaller than the major moons of Jupiter and Saturn. Earth is about 80 times the volume than the Moon, but both are about the same age. A prevailing theory is that the Moon was once part of the Earth, and was formed from a chunk that broke away due to a huge object colliding with Earth when it was relatively young.";
+    private String moonFact5 = "During the 1950’s the USA considered detonating a nuclear bomb on the Moon. The secret project was during the height cold war was known as “A Study of Lunar Research Flights” or “Project A119” and meant as a show of strength at a time they were lagging behind in the space race.";
+    private String marsFact1 = "The largest canyon system in the solar system is Valles Marineris on Mars. It’s more than 4,000 kilometers (3,000 miles) long — enough to stretch from California to New York. It is nine times as long and four times as deep as Earth’s Grand Canyon!";
+    private String marsFact2 = "If you could stand at the Martian equator, the temperature at your feet would be like a warm spring day, but at your head it would be freezing cold!";
+    private String marsFact3 = "Olympus Mons, a shield volcano, is 21km high and 600km in diameter. Despite having formed over billions of years, evidence from volcanic lava flows is so recent many scientists believe it could still be active.";
+    private String marsFact4 = "Mars has the largest dust storms in the solar system. They can last for months and cover the entire planet. The seasons are extreme because its elliptical (oval-shaped) orbital path around the Sun is more elongated than most other planets in the solar system.";
+    private String marsFact5 = "One day Mars will have a ring. In the next 20-40 million years Mars’ largest moon Phobos will be torn apart by gravitational forces leading to the creation of a ring that could last up to 100 million years.";
+    private String beltFact1 = "Ceres is the largest, most massive body in the main asteroid belt between Mars and Jupiter, totaling about a third of the total mass of the entire belt. But Ceres is the smallest of the dwarf planets, which include Pluto and Eris, and the only dwarf planet that resides in the asteroid belt";
+    private String beltFact2 = "If you could lump together all the thousands of known asteroids in our solar system, their total mass wouldn’t even equal 10 percent of the mass of Earth’s Moon.in the asteroid belt";
+    private String beltFact3 = "The Asteroid Belt is often referred to as the “Main Belt” to distinguish it from other groups of asteroids such as the Lagrangians and Centaurs.";
     
     
     public void pickATalky(int day){
